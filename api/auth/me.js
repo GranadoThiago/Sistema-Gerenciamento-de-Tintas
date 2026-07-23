320b1e07c5fc6c48be1440257894806efcd13374
DevDias <110613133+GranadoThiago@users.noreply.github.com>
2026-07-23 07:58:47 -0300

    Excluir me.js

--- api/auth/me.js
+++
@@ -1,9 +0,0 @@
-const { getUserFromRequest } = require('../_lib/auth');
-
-module.exports = async (req, res) => {
-  const user = getUserFromRequest(req);
-  if (!user) {
-    return res.status(401).json({ erro: 'Não autenticado' });
-  }
-  res.status(200).json(user);
-};
