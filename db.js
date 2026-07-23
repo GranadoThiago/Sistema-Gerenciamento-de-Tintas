23f65a9038cdc8eae6f91a3bdc41b5b73103967d
GranadoThiago <learn.now.e@gmail.com>
2026-07-23 08:09:24 -0300

    update

--- db.js
+++
@@ -1,5 +0,0 @@
-// Usa a variável de ambiente POSTGRES_URL, criada automaticamente pela Vercel
-// quando você adiciona o armazenamento "Postgres" (Neon) ao projeto.
-const { sql } = require('@vercel/postgres');
-
-module.exports = { sql };
